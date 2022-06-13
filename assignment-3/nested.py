bd439660c3dfb2846f84947cc3b42ee73a484896
pritam ramteke <pritam.teke@exceloid.com>
2022-06-13 16:04:03 +0530

    new changes add

--- assignment-3/nested.py
+++ assignment-3/nested.py
@@ -1,9 +1,9 @@
-def get_in(value, key):
+def get_value(value, key):
     keys = key.split('/')
     for k in keys:
         value = value.pop(k)
     return value
 
-object = {"x1":{"y1":"{"z1":"a1"}"}}
-key = x1/y1/z1
-print (get_in(object, key))
+object = {"a": { "b": { "c": "d" }}}
+key = 'a/b/c'
+print ("Result of x1/y1/z1:--", get_value(object, key))
